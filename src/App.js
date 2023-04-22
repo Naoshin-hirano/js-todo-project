baa0ef35fdefa3f6c767894f7bc98bcfec8fe446
Hirano <hnaoya1120@gmail.com>
2023-04-23 02:06:45 +0900

    submitするとログに入力内容が表示

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 console.log("App.js: loaded");
 export class App {
-    constructor() {
-        console.log("App initialized");
+    mount() {
+        if (typeof window !== "undefined") {
+            const formElement = window.document.querySelector("#js-form");
+            const inputElement = window.document.querySelector(
+                "#js-form-input"
+            );
+            formElement.addEventListener("submit", (event) => {
+                // preventDefaultしないとページがリロードされてしまう
+                event.preventDefault();
+                console.log(`入力欄の値: ${inputElement.value}`);
+            });
+        }
     }
 }
